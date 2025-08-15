42774fbc26007e266ee7b4553e59e1bc31ab7a3b
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-08-15 18:37:45 +0530

    update colors

--- lib/theme/color_theme.dart
+++ lib/theme/color_theme.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xFFFFB900);
 const Color primaryColorLight = Color(0xFFF8D984);
 const Color primaryColorDark = Color(0xFFE28E00);
+const Color primaryColorIcon = Color(0xFFFFA000);
 
 // black colors for the app
 const Color blackPrimary = Color(0xFF000000);
